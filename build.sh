56654f79acae3e55ae57b0703a89866434e8fb15
devlexander <infinityen@protonmail.com>
2020-09-01 21:04:47 +0100

    edit build configuration

--- build.sh
+++ build.sh
@@ -5,13 +5,17 @@ then
 	rm -rf builds
 fi
 
+echo "Building..."
+
 ## Build for linux..
 GOOS=linux GOARCH=amd64 go build -o builds/linux/touch64 main.go
 GOOS=linux GOARCH=386 go build -o builds/linux/touch32 main.go 
 
-## Build for macOS.
-GOOS=darwin GOARCH=amd64 go build -o builds/darwin/touch64 main.go
+## Build for darwin.
+GOOS=darwin GOARCH=amd64 go build -o builds/darwin/touch64-darwin main.go
 
 ## Build for Windows.
 GOOS=windows GOARCH=amd64 go build -o builds/win/touch64.exe main.go
 GOOS=windows GOARCH=386 go build -o builds/win/touch32.exe main.go
+
+echo "Complete."
